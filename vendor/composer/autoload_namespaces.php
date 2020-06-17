6718f1d84ddcefb8017196157b573cb035267028
iulianstave <iulian.staverescu@gmail.com>
2020-06-17 14:58:13 +0300

    Update core to 8.9.0

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,8 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'org\\bovigo\\vfs\\' => array($vendorDir . '/mikey179/vfsstream/src/main/php'),
-    'WebDriver' => array($vendorDir . '/instaclick/php-webdriver/lib'),
     'Twig_' => array($vendorDir . '/twig/twig/lib'),
     'Stack' => array($vendorDir . '/stack/builder/src'),
     'EasyRdf_' => array($vendorDir . '/easyrdf/easyrdf/lib'),
